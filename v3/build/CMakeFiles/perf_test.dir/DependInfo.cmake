896e12ce6431ac021f5d9a8ef111a05a0c07d7f1
WW <erhewang2024@163.com>
2025-06-21 09:17:12 +0800

    complete test

---
+++ v3/build/CMakeFiles/perf_test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/src/CentralCache.cpp" "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/build/CMakeFiles/perf_test.dir/src/CentralCache.cpp.o"
+  "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/src/PageCache.cpp" "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/build/CMakeFiles/perf_test.dir/src/PageCache.cpp.o"
+  "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/src/ThreadCache.cpp" "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/build/CMakeFiles/perf_test.dir/src/ThreadCache.cpp.o"
+  "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/tests/PerformanceTest.cpp" "/home/ww/GitCode/github/WorkProjects/CPPmemory-pool/v3/build/CMakeFiles/perf_test.dir/tests/PerformanceTest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ v3/build/CMakeFiles/unit_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_test.dir/src/CentralCache.cpp.o"
+  "CMakeFiles/unit_test.dir/src/PageCache.cpp.o"
+  "CMakeFiles/unit_test.dir/src/ThreadCache.cpp.o"
+  "CMakeFiles/unit_test.dir/tests/UnitTest.cpp.o"
+  "unit_test"
+  "unit_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
